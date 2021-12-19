6540d09c284da09e877243b632f9838a846f055e
atombo0511 <totrungatom@gmail.com>
2021-12-19 17:28:50 +0700

    topsis + dsgt

---
+++ src/Info_Author.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trung Atom
+ */
+public class Info_Author {
+    public int ID_Author;
+    public String name_Author;
+    public String email;
+}
---
+++ src/Info_Lesson.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trung Atom
+ */
+public class Info_Lesson {
+    public int ID_lesson;
+    public String name_lesson;
+    public String name_author;
+    public String name_depart;
+}
